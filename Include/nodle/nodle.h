e2c2e4c4ffe8507a62ea6f2025e955d79771a1f3
Patrik Nyblad <patrik.nyblad@gmail.com>
2019-02-01 18:13:40 +0100

    Move include folder to root of repository and create a separate source folder

--- Include/nodle/nodle.h
+++ Include/nodle/nodle.h
@@ -1,5 +1,6 @@
 #import <UIKit/UIKit.h>
 
+// Base classes
 #import "Node.h"
 #import "NodeInput.h"
 #import "NodeOutput.h"
@@ -8,12 +9,9 @@
 #import "RGBNode.h"
 #import "AssembleColorNode.h"
 #import "UpdateBackgroundColorNode.h"
+
 //! Project version number for nodle.
 FOUNDATION_EXPORT double nodleVersionNumber;
 
 //! Project version string for nodle.
 FOUNDATION_EXPORT const unsigned char nodleVersionString[];
-
-// In this header, you should import all the public headers of your framework using statements like #import <nodle/PublicHeader.h>
-
-
