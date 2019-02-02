f59d8c44e7839e502b19943e452df4ee42711fe5
Mikael Sundström <mikael@sundstrom.st>
2019-02-02 10:10:21 +0100

    Node tweaking
    Made input and output properties public
    Changed the input trigger of the RGB-node from “Any” to “All”

--- Include/nodle/AssembleColorNode.h
+++ Include/nodle/AssembleColorNode.h
@@ -7,6 +7,12 @@ NS_ASSUME_NONNULL_BEGIN
  */
 @interface AssembleColorNode : AbstractNode
 
+@property (nonatomic, strong) NodeInput *rInput;
+@property (nonatomic, strong) NodeInput *gInput;
+@property (nonatomic, strong) NodeInput *bInput;
+
+@property (nonatomic, strong) NodeOutput *colorOutput;
+
 @end
 
 NS_ASSUME_NONNULL_END
